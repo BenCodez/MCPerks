1cbc86fc7039004a2f5e55cd729a6b181b249925
Ben12345rocks <benbergen12@gmail.com>
2018-12-30 13:14:10 -0500

    Added increase strength effect

--- MCPerks/src/com/Ben12345rocks/MCPerks/Perk/Effect.java
+++ MCPerks/src/com/Ben12345rocks/MCPerks/Perk/Effect.java
@@ -67,7 +67,9 @@ public enum Effect {
 
 	IncreaseMaxHealth,
 
-	MoveSpeed;
+	MoveSpeed,
+
+	IncreaseStrength;
 
 	public static Effect fromString(String str) {
 		for (Effect eff : values()) {
@@ -93,29 +95,38 @@ public enum Effect {
 			}
 		}
 		switch (this) {
-		case Fly:
-			new FlyEffect().disableFly(players);
-			break;
-		case IncreaseMaxHealth:
-			for (Player p : players) {
-				for (AttributeModifier modifier : p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getModifiers()) {
-					if (modifier.getName().equalsIgnoreCase("Increase health")) {
-						p.getAttribute(Attribute.GENERIC_MAX_HEALTH).removeModifier(modifier);
+			case Fly:
+				new FlyEffect().disableFly(players);
+				break;
+			case IncreaseMaxHealth:
+				for (Player p : players) {
+					for (AttributeModifier modifier : p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getModifiers()) {
+						if (modifier.getName().equalsIgnoreCase("Increase health")) {
+							p.getAttribute(Attribute.GENERIC_MAX_HEALTH).removeModifier(modifier);
+						}
 					}
 				}
-			}
-			break;
-		case MoveSpeed:
-			for (Player p : players) {
-				for (AttributeModifier modifier : p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getModifiers()) {
-					if (modifier.getName().equalsIgnoreCase("Increase speed")) {
-						p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).removeModifier(modifier);
+				break;
+			case IncreaseStrength:
+				for (Player p : players) {
+					for (AttributeModifier modifier : p.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getModifiers()) {
+						if (modifier.getName().equalsIgnoreCase("Increase strength")) {
+							p.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).removeModifier(modifier);
+						}
 					}
 				}
-			}
-			break;
-		default:
-			break;
+				break;
+			case MoveSpeed:
+				for (Player p : players) {
+					for (AttributeModifier modifier : p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getModifiers()) {
+						if (modifier.getName().equalsIgnoreCase("Increase speed")) {
+							p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).removeModifier(modifier);
+						}
+					}
+				}
+				break;
+			default:
+				break;
 
 		}
 	}
@@ -146,113 +157,119 @@ public enum Effect {
 			}
 		}
 		switch (this) {
-		case CureHunger:
-			Bukkit.getScheduler().runTask(Main.plugin, new Runnable() {
+			case CureHunger:
+				Bukkit.getScheduler().runTask(Main.plugin, new Runnable() {
 
-				@Override
-				public void run() {
-					for (Player player : players) {
-						player.setFoodLevel(20);
+					@Override
+					public void run() {
+						for (Player player : players) {
+							player.setFoodLevel(20);
+						}
+					}
+				});
+				break;
+			case HealAll:
+				Bukkit.getScheduler().runTask(Main.plugin, new Runnable() {
+
+					@Override
+					public void run() {
+						for (Player player : players) {
+							player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+						}
+					}
+				});
+				break;
+			case CureSpells:
+				for (Player player : players) {
+					if (player.hasPotionEffect(PotionEffectType.BLINDNESS)) {
+						player.removePotionEffect(PotionEffectType.BLINDNESS);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.CONFUSION)) {
+						player.removePotionEffect(PotionEffectType.CONFUSION);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.HARM)) {
+						player.removePotionEffect(PotionEffectType.HARM);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.POISON)) {
+						player.removePotionEffect(PotionEffectType.POISON);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.SLOW)) {
+						player.removePotionEffect(PotionEffectType.SLOW);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.SLOW_DIGGING)) {
+						player.removePotionEffect(PotionEffectType.SLOW_DIGGING);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.WEAKNESS)) {
+						player.removePotionEffect(PotionEffectType.WEAKNESS);
+					}
+
+					if (player.hasPotionEffect(PotionEffectType.WITHER)) {
+						player.removePotionEffect(PotionEffectType.WITHER);
 					}
 				}
-			});
-			break;
-		case HealAll:
-			Bukkit.getScheduler().runTask(Main.plugin, new Runnable() {
-
-				@Override
-				public void run() {
-					for (Player player : players) {
-						player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+				break;
+			case Fly:
+				new FlyEffect().enableFly(perk.getFlyWorlds(), players);
+				break;
+			case Potions:
+				Set<String> potions = perk.getPotions();
+				if (potions != null) {
+					for (String potion : potions) {
+						new PotionEffect().giveEffect(potion, perk.getPotionDurations().get(potion),
+								perk.getPotionAmplifiers().get(potion), players);
 					}
 				}
-			});
-			break;
-		case CureSpells:
-			for (Player player : players) {
-				if (player.hasPotionEffect(PotionEffectType.BLINDNESS)) {
-					player.removePotionEffect(PotionEffectType.BLINDNESS);
+
+				break;
+			case Rewards:
+				for (Player p : players) {
+					new RewardBuilder(ConfigPerks.getInstance().getData(perk.getPerk()),
+							ConfigPerks.getInstance().getPerkRewardsPath(perk.getPerk()))
+									.send(UserManager.getInstance().getMCPerksUser(p));
+
 				}
 
-				if (player.hasPotionEffect(PotionEffectType.CONFUSION)) {
-					player.removePotionEffect(PotionEffectType.CONFUSION);
+				break;
+			case Commands:
+				for (Player p : players) {
+					HashMap<String, String> placeholders = new HashMap<String, String>();
+					placeholders.put("player", p.getName());
+					MiscUtils.getInstance().executeConsoleCommands(p, perk.getCommands(), placeholders);
 				}
-
-				if (player.hasPotionEffect(PotionEffectType.HARM)) {
-					player.removePotionEffect(PotionEffectType.HARM);
+				break;
+			case IncreaseMaxHealth:
+				for (Player p : players) {
+					p.getAttribute(Attribute.GENERIC_MAX_HEALTH)
+							.addModifier(new AttributeModifier("Increase health", getModifier(), Operation.ADD_NUMBER));
 				}
-
-				if (player.hasPotionEffect(PotionEffectType.POISON)) {
-					player.removePotionEffect(PotionEffectType.POISON);
+				break;
+			case IncreaseStrength:
+				for (Player p : players) {
+					p.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).addModifier(
+							new AttributeModifier("Increase strength", getModifier(), Operation.ADD_NUMBER));
 				}
-
-				if (player.hasPotionEffect(PotionEffectType.SLOW)) {
-					player.removePotionEffect(PotionEffectType.SLOW);
+				break;
+			case MoveSpeed:
+				for (Player p : players) {
+					p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).addModifier(
+							new AttributeModifier("Increase speed", getModifier(), Operation.MULTIPLY_SCALAR_1));
 				}
-
-				if (player.hasPotionEffect(PotionEffectType.SLOW_DIGGING)) {
-					player.removePotionEffect(PotionEffectType.SLOW_DIGGING);
-				}
-
-				if (player.hasPotionEffect(PotionEffectType.WEAKNESS)) {
-					player.removePotionEffect(PotionEffectType.WEAKNESS);
-				}
-
-				if (player.hasPotionEffect(PotionEffectType.WITHER)) {
-					player.removePotionEffect(PotionEffectType.WITHER);
-				}
-			}
-			break;
-		case Fly:
-			new FlyEffect().enableFly(perk.getFlyWorlds(), players);
-			break;
-		case Potions:
-			Set<String> potions = perk.getPotions();
-			if (potions != null) {
-				for (String potion : potions) {
-					new PotionEffect().giveEffect(potion, perk.getPotionDurations().get(potion),
-							perk.getPotionAmplifiers().get(potion), players);
-				}
-			}
-
-			break;
-		case Rewards:
-			for (Player p : players) {
-				new RewardBuilder(ConfigPerks.getInstance().getData(perk.getPerk()),
-						ConfigPerks.getInstance().getPerkRewardsPath(perk.getPerk()))
-								.send(UserManager.getInstance().getMCPerksUser(p));
-
-			}
-
-			break;
-		case Commands:
-			for (Player p : players) {
-				HashMap<String, String> placeholders = new HashMap<String, String>();
-				placeholders.put("player", p.getName());
-				MiscUtils.getInstance().executeConsoleCommands(p, perk.getCommands(), placeholders);
-			}
-			break;
-		case IncreaseMaxHealth:
-			for (Player p : players) {
-				p.getAttribute(Attribute.GENERIC_MAX_HEALTH)
-						.addModifier(new AttributeModifier("Increase health", getModifier(), Operation.ADD_NUMBER));
-			}
-			break;
-		case MoveSpeed:
-			for (Player p : players) {
-				p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).addModifier(
-						new AttributeModifier("Increase speed", getModifier(), Operation.MULTIPLY_SCALAR_1));
-			}
-			break;
-		default:
-			break;
+				break;
+			default:
+				break;
 
 		}
 	}
 
 	public boolean usesModifier() {
 		return isEffect(Effect.IncreaseMobDrops, Effect.DoubleExperience, Effect.Farmer, Effect.Fortune, Effect.McmmoXP,
-				Effect.HeadDropper);
+				Effect.HeadDropper, Effect.IncreaseStrength);
 	}
 
 	public boolean isEffect(Effect... effects) {
