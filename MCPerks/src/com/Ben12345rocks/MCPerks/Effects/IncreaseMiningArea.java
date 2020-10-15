448d9e33da79c7cca729724dfb9755fa7e640141
Ben12345rocks <benbergen12@gmail.com>
2020-10-15 19:08:40 -0400

    Made block breaks check build perms

--- MCPerks/src/com/Ben12345rocks/MCPerks/Effects/IncreaseMiningArea.java
+++ MCPerks/src/com/Ben12345rocks/MCPerks/Effects/IncreaseMiningArea.java
@@ -3,6 +3,9 @@
  */
 package com.Ben12345rocks.MCPerks.Effects;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -44,18 +47,33 @@ public class IncreaseMiningArea {
 		if (xdirection) {
 			for (int x = (int) (event.getBlock().getX() - range); x <= event.getBlock().getX() + range; x++) {
 				for (int y = (int) (event.getBlock().getY() - range); y <= event.getBlock().getY() + range; y++) {
-					event.getBlock().getWorld().getBlockAt(x, y, event.getBlock().getZ())
-							.breakNaturally(itemInHand);
+					Block b = event.getBlock().getWorld().getBlockAt(x, y, event.getBlock().getZ());
+
+					if (!b.getType().equals(Material.BEDROCK) && canBreakBlock(p, b)) {
+						b.breakNaturally(itemInHand);
+					}
 				}
 			}
 		} else {
 			for (int z = (int) (event.getBlock().getZ() - range); z <= event.getBlock().getZ() + range; z++) {
 				for (int y = (int) (event.getBlock().getY() - range); y <= event.getBlock().getY() + range; y++) {
-					event.getBlock().getWorld().getBlockAt(event.getBlock().getX(), y, z)
-							.breakNaturally(itemInHand);
+					Block b = event.getBlock().getWorld().getBlockAt(event.getBlock().getX(), y, z);
+
+					if (!b.getType().equals(Material.BEDROCK) && canBreakBlock(p, b)) {
+						b.breakNaturally(itemInHand);
+					}
 				}
 			}
 		}
+	}
+
+	public boolean canBreakBlock(Player p, Block b) {
+		BlockBreakEvent block = new BlockBreakEvent(b, p);
+		Bukkit.getPluginManager().callEvent(block);
+		if (!block.isCancelled()) {
+			return true;
+		}
+		return false;
 	}
 
 }
