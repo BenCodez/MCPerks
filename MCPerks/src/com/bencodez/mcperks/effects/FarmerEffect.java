26a67bc7ec0d575a7d8ee39e2d12f67a4e0be9b4
BenCodez <benbergen12@gmail.com>
2021-08-10 14:45:50 -0400

    Add betroots to farmer effects

--- MCPerks/src/com/bencodez/mcperks/effects/FarmerEffect.java
+++ MCPerks/src/com/bencodez/mcperks/effects/FarmerEffect.java
@@ -45,6 +45,7 @@ public class FarmerEffect {
 	 */
 	public boolean isFarmable(Block b) {
 		return b.getType().equals(Material.POTATOES) || b.getType().equals(Material.CARROTS)
-				|| b.getType().equals(Material.WHEAT) || b.getType().equals(Material.WHEAT_SEEDS);
+				|| b.getType().equals(Material.WHEAT) || b.getType().equals(Material.WHEAT_SEEDS)
+				|| b.getType().equals(Material.BEETROOTS);
 	}
 }
